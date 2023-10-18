2184fb722f9ef34666e27d5a9c9f3ebe90a3b047
kjh <kpp0819@gmail.com>
2023-10-18 12:36:02 +0900

    쓸모없는 주석 정리

--- Kiosk_UI/Form1.cs
+++ Kiosk_UI/Form1.cs
@@ -14,6 +14,7 @@ using System.Collections;
 
 namespace Kiosk_UI
 {    
+
     public partial class MainForm : Form
     {
         public MainForm()
@@ -105,41 +106,6 @@ namespace Kiosk_UI
                     }
                 }
             }
-            Search("우유",true);
-        ////음료
-        //AddItem("아메리카노", 2000, categories.drink, "americano.png",detail:new string[]{ "커피","카페인"});
-        //AddItem("에스프레소", 2000, categories.drink, "espresso.png");
-        //AddItem("카푸치노", 3000, categories.drink, "cappuccino.png");
-        //AddItem("녹차라떼", 3500, categories.drink, "greentealatte.png");
-        //AddItem("아이스초코", 2500, categories.drink, "icedchocolate.png");
-        //AddItem("카페라떼", 2500, categories.drink, "cafelatte.png");
-        //AddItem("카라멜 마끼아또", 2500, categories.drink, "caramelmacchiato.png");
-        //AddItem("커피 플렛치노", 2500, categories.drink, "coffeeflatccino.png");
-        //AddItem("아이스티", 2500, categories.drink, "icedtea.png");
-        //AddItem("밀크티", 2500, categories.drink, "milktea.png");
-        //AddItem("요거트 플렛치노", 2500, categories.drink, "yogurtflatccino.png");
-        //AddItem("민트초코", 2500, categories.drink, "mintchocolate.png");
-        //AddItem("아인슈페너", 2500, categories.drink, "einspenner.png");
-        //AddItem("레몬그라스차", 2500, categories.drink, "lemongrasstea.png");
-        //AddItem("페퍼민트차", 2500, categories.drink, "pepperminttea.png");
-        //AddItem("로즈마리차", 2500, categories.drink, "rosemarytea.png");
-        //AddItem("캐모마일차", 2500, categories.drink, "chamomiletea.png");
-
-            ////디저트
-            //AddItem("베이글", 1500, categories.dessert, "bagel.png");
-            //AddItem("블루베리 베이글", 1500, categories.dessert, "blueberrybagel.png");
-            //AddItem("조각 치즈케이크", 1500, categories.dessert, "cheesecake.png");
-            //AddItem("조각 초콜릿케이크", 1500, categories.dessert, "chocolatecake.png");
-            //AddItem("조각 딸기케이크", 1500, categories.dessert, "strawberrycake.png");
-            //AddItem("초코칩쿠키", 1500, categories.dessert, "cookie.png");
-            //AddItem("더블초코칩 쿠키", 1500, categories.dessert, "doublecookie.png");
-            //AddItem("녹차 쿠키", 1500, categories.dessert, "greenteacookie.png");
-            //AddItem("허니브레드", 1500, categories.dessert, "honeybread.png");
-            //AddItem("딸기마카롱", 1500, categories.dessert, "strawberrymacaron.png");
-            //AddItem("초코마카롱", 1500, categories.dessert, "chocomacaron.png");
-            //AddItem("녹차마카롱", 1500, categories.dessert, "greenteamacaron.png");
-
-
         }
 
         private void AllmenuButton_Click(object sender, EventArgs e)
@@ -232,11 +198,6 @@ namespace Kiosk_UI
                             searchResults = Result;
                             flagForSearch = true;
                         }
-                        foreach (var tmp in searchResults)
-                        {
-                            Console.WriteLine($"{tmp}");
-                        }
-                        Console.WriteLine("\n");
                     }
 
                 }
@@ -260,7 +221,15 @@ namespace Kiosk_UI
                         if (text == control.Title)
                             control.Visible = true;
                     }
+                    foreach (var text in searchResults)
+                    {
+                        tts.Speak(text);
+                    }
                 }
+            }
+            if (searchResults.Count == 0)
+            {
+                tts.Speak("죄송합니다 메뉴를 찾을수 없었습니다.");
             }
         }
 
