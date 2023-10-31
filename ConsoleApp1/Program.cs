0a1a07aa6c3f06ef28693bacc34eaef949d2b1fc
kjh <kpp0819@gmail.com>
2023-10-31 15:15:45 +0900

    test for face lib

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FaceRecognition;
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var face= new FaceRecognition.FaceRecognition();
+            
+            while(true) {
+                var faceLocate = face.Recognition().Result;
+                Console.WriteLine(faceLocate);
+            }
+        }
+    }
+}
