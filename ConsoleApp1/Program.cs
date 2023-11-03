0a3c46fd4798f8c4539f768428b7b3b1dc640a99
kjh <kpp0819@gmail.com>
2023-11-03 23:57:34 +0900

    얼굴인식 테스트 개선

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,13 +8,26 @@ namespace ConsoleApp1
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             var face= new FaceRecognition.FaceRecognition();
             
             while(true) {
-                var faceLocate = face.Recognition().Result;
+                var faceLocate = await face.Recognition();
                 Console.WriteLine(faceLocate);
+                if (Int32.TryParse(faceLocate, out var result))
+                {
+                    if (result > 400)
+                        Console.WriteLine("HighDistance");
+                    else if (result > 300)
+                        Console.WriteLine("1 4000");
+                    else if (result > 200)
+                        Console.WriteLine("1 3000");
+                    else if (result > 100)
+                        Console.WriteLine("1 2000");
+                    else
+                        Console.WriteLine("1 1000");
+                }
             }
         }
     }
