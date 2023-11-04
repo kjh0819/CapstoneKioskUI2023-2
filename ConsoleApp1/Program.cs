993552bc648c523d688d909fae3f5dea0073db71
진훈 김 <kpp0819@naver.com>
2023-11-04 23:06:02 +0900

    얼굴인식 테스트용 코드 수정

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,34 +1,69 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using FaceRecognition;
+using static System.Net.Mime.MediaTypeNames;
+
 namespace ConsoleApp1
 {
-    internal class Program
+    public class SerialFromArduino
     {
-        static async Task Main(string[] args)
+        public void Main() 
         {
-            var face= new FaceRecognition.FaceRecognition();
-            
-            while(true) {
-                var faceLocate = await face.Recognition();
-                Console.WriteLine(faceLocate);
-                if (Int32.TryParse(faceLocate, out var result))
+            SerialPort port1;
+            string receivedData = "";
+            port1 = new SerialPort("COM5");
+
+            port1.DataReceived += (sender, e) =>
+            {
+                SerialPort port = (SerialPort)sender;
+                string data = port.ReadTo("\n");
+                switch(data)
                 {
-                    if (result > 400)
-                        Console.WriteLine("HighDistance");
-                    else if (result > 300)
-                        Console.WriteLine("1 4000");
-                    else if (result > 200)
-                        Console.WriteLine("1 3000");
-                    else if (result > 100)
-                        Console.WriteLine("1 2000");
-                    else
-                        Console.WriteLine("1 1000");
+                    case ("on"):
+                        var face = new FaceRecognition.FaceRecognition();
+                        string faceLocate="";
+                        for(int i = 0; i<20;i++)
+                        {
+                            faceLocate = face.Recognition().Result;
+                            Console.WriteLine(faceLocate);
+
+                            if (faceLocate != "error")
+                                break;
+                        }
+                        if (Int32.TryParse(faceLocate, out var result))
+                        {
+                            if (result > 400)
+                                port1.WriteLine("1 4000");
+                            else if (result > 300)
+                                port1.WriteLine("1 3000");
+                            else if (result > 200)
+                                port1.WriteLine("1 2000");
+                            else if (result > 100)
+                                port1.WriteLine("1 1000");
+                        }
+                        break;
+                    case ("off"):
+                        port1.WriteLine("2 5000");
+                        break;
+                    default: 
+                        break;
                 }
-            }
+                Console.WriteLine(data);
+            };
+            port1.BaudRate = 9600;
+            port1.DataBits = 8;
+            port1.StopBits = StopBits.One;
+            port1.Parity = Parity.None;
+            port1.ReadTimeout = 1000;
+            port1.Open();
+
+            Console.WriteLine("Press Enter to quit");
+            Console.ReadLine();
         }
     }
+    
 }
