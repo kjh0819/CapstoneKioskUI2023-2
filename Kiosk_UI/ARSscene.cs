f397336d0e9e4bc9a4bd367f81e8303ca95d71fd
KMZ150 <147787809+KMZ150@users.noreply.github.com>
2023-11-10 12:10:57 +0900

    test

--- Kiosk_UI/ARSscene.cs
+++ Kiosk_UI/ARSscene.cs
@@ -1,96 +1,119 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TTSLib;
 using System.Windows.Forms;
+using TTSLib;
 
 namespace Kiosk_UI
 {
+
     internal class ARSscene
     {
 
-
+        TextToSpeechConverter tts = new TextToSpeechConverter();
         public static int key_flag = 0;
         public static int key_flag2 = 0;
         public static void ARS1(KeyEventArgs e)
         {
-            TextToSpeechConverter tts = new TextToSpeechConverter();
+            ARSscene ars = new ARSscene();
             //1번 시나리오
             if (e.KeyCode >= Keys.NumPad0 && e.KeyCode <= Keys.NumPad9 && key_flag == 0)
             {
-                tts.StopSpeak();
-                tts.Speak("음료를 원하시면 일번을, 디저트를 원하시면 이번을, " +
-                    "음성검색을 원하시면 삼번을 눌러주세요. 다시 듣고싶다면 영번, 종료하고싶으시면 구번을 눌러주세요.");
+                ars.ARSstart(); //ARS시작
                 key_flag = 1;
             }
-            else if (e.KeyCode == Keys.NumPad1 && key_flag == 1)
+            else if (e.KeyCode == Keys.NumPad1 && key_flag == 1) //1번 누를시
             {
-                tts.StopSpeak();
-                ARSdrink();
-                if(e.KeyCode == Keys.NumPad9)
-                {
-                    ARS1(e);
-                }
+                ars.ARSdrink(); //커피 논커피선택
+                key_flag = 2;
             }
+            else if (e.KeyCode == Keys.NumPad9 && key_flag == 2) //9번 누를시
+            {
+                ars.ARSstart();  //처음으로
+            }
+            else if (e.KeyCode == Keys.NumPad1 && key_flag == 2) //1번 누를시
+            {
+                ars.ARScoffee(); //우유커피 선택
+            }
+            else if (e.KeyCode == Keys.NumPad2 && key_flag == 2) //2번 누를시
+            {
+                ars.ARSnoncoffee(); //우유논커피 선택
+            }
+
             else if (e.KeyCode == Keys.NumPad2 && key_flag == 1)
-            { tts.StopSpeak();
-                ARSdessert(); }
-            
+            {
+                ars.ARSdessert();  //디저트선택
+                key_flag = 2;
+                if (e.KeyCode == Keys.NumPad1 && key_flag == 2)
+                {
+                    Console.WriteLine("빵을 선택하셨습니다.");
+                }
+                else if (e.KeyCode == Keys.NumPad2 && key_flag == 2)
+                {
+                    Console.WriteLine("쿠키를 선택하셨습니다");
+                }
+
+            }
+
+
         }
         //2번 시나리오
-        public static void ARSdrink()
+        public void ARSstart()
         {
-            TextToSpeechConverter tts = new TextToSpeechConverter();
+            tts.StopSpeak();
+            tts.SpeakSynchronous("음료를 원하시면 일번을, 디저트를 원하시면 이번을, " +
+                   "음성검색을 원하시면 삼번을 눌러주세요. 다시 듣고싶다면 영번, 종료하고싶으시면 구번을 눌러주세요.");
+        }
+        public void ARSdrink()
+        {
+            tts.StopSpeak();
+            tts.SpeakSynchronous("커피를 원하시면 일번, 커피가 아닌 음료를 원하시면 이번을 눌러주세요. 다시듣고싶다면 팔번을, 돌아가기는 구번을 눌러주세요.");
+        }
+        public void ARScoffee()
+        {
+            tts.StopSpeak();
+            tts.Speak("우유가 들어간 커피를 원하시면 일번을, 아닌 커피는 이번을 눌러주세요.");
+
+        }
+        public void ARSmilkcoffee()
+        {
             {
-                tts.Speak("커피를 원하시면 일번, 커피가 아닌 음료를 원하시면 이번을 눌러주세요. 다시듣고싶다면 팔번을, 돌아가기는 구번을 눌러주세요.");
+
             }
         }
-        public static void ARScoffee()
+        public void ARSnoncoffee()
         {
-            TextToSpeechConverter tts = new TextToSpeechConverter();
-            {
-                tts.Speak("우유가 들어간 커피를 원하시면 일번을, 아닌 커피는 이번을 눌러주세요.");
-            }
-        }
-        public static void ARSnoncoffee()
-        {
-            var tts = new TextToSpeechConverter();
             {
                 tts.Speak("우유가 들어간 음료는 일번을, 아닌 음료는 이번을 눌러주세요.");
             }
         }
-        public static void ARSdessert()
+        public void ARSdessert()
         {
-            var tts = new TextToSpeechConverter();
             {
                 tts.Speak("빵류를 원하시면 일번, 과자류를 원하시면 이번을 눌러주세요.다시듣고싶다면 팔번을, 돌아가기는 구번을 눌러주세요.");
             }
         }
 
-        public static void ARSvoice(KeyEventArgs e)
+        public void ARSvoice(KeyEventArgs e)
         {
-            
-        }
-                /*else if (e.KeyCode == Keys.NumPad2 && key_flag == 1)
-                {
-                    key_flag = 2; key_flag2 = 2;
-                    tts.StopSpeak(); //디저트
-                    tts.StopSpeak();
-                    tts.Speak("빵류를 원하시면 일번, 과자류를 원하시면 이번을 눌러주세요.");
-                }
-                else if (e.KeyCode == Keys.NumPad3 && key_flag == 1)
-                {
-                    key_flag = 2; key_flag2 = 3; //음성인식
-                }
-                else if (e.KeyCode == Keys.NumPad0 && key_flag == 1)
-                {
-                    key_flag = 0; tts.StopSpeak();
 
-                }*/
-            }
         }
+        /*else if (e.KeyCode == Keys.NumPad2 && key_flag == 1)
+        {
+            key_flag = 2; key_flag2 = 2;
+            tts.StopSpeak(); //디저트
+            tts.StopSpeak();
+            tts.Speak("빵류를 원하시면 일번, 과자류를 원하시면 이번을 눌러주세요.");
+        }
+        else if (e.KeyCode == Keys.NumPad3 && key_flag == 1)
+        {
+            key_flag = 2; key_flag2 = 3; //음성인식
+        }
+        else if (e.KeyCode == Keys.NumPad0 && key_flag == 1)
+        {
+            key_flag = 0; tts.StopSpeak();
 
-    
+        }*/
+    }
+}
+
+
 
--- Kiosk_UI/warning.Designer.cs
+++ Kiosk_UI/warning.Designer.cs
@@ -41,11 +41,10 @@
             this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Noto Sans KR", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label1.Location = new System.Drawing.Point(35, 111);
+            this.label1.Location = new System.Drawing.Point(1, 146);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(279, 30);
+            this.label1.Size = new System.Drawing.Size(385, 30);
             this.label1.TabIndex = 0;
             this.label1.Text = "아무것도 선택되지 않았습니다!";
             this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -55,13 +54,13 @@
             this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Noto Sans KR", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label2.Location = new System.Drawing.Point(100, 157);
+            this.label2.Location = new System.Drawing.Point(-1, 189);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(149, 24);
+            this.label2.Size = new System.Drawing.Size(389, 24);
             this.label2.TabIndex = 1;
             this.label2.Text = "메뉴를 선택해주세요";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // custom_button1
             // 
@@ -73,9 +72,9 @@
             this.custom_button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.custom_button1.Font = new System.Drawing.Font("Noto Sans KR", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.custom_button1.ForeColor = System.Drawing.Color.White;
-            this.custom_button1.Location = new System.Drawing.Point(92, 206);
+            this.custom_button1.Location = new System.Drawing.Point(92, 237);
             this.custom_button1.Name = "custom_button1";
-            this.custom_button1.Size = new System.Drawing.Size(167, 45);
+            this.custom_button1.Size = new System.Drawing.Size(220, 61);
             this.custom_button1.TabIndex = 2;
             this.custom_button1.Text = "확인";
             this.custom_button1.UseVisualStyleBackColor = false;
@@ -85,9 +84,9 @@
             // 
             this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(136, 28);
+            this.pictureBox1.Location = new System.Drawing.Point(146, 28);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(73, 70);
+            this.pictureBox1.Size = new System.Drawing.Size(97, 102);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 4;
             this.pictureBox1.TabStop = false;
@@ -96,18 +95,18 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(334, 301);
+            this.ClientSize = new System.Drawing.Size(387, 346);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.custom_button1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "warning";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
             this.Text = "warning";
             this.Load += new System.EventHandler(this.warning_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
