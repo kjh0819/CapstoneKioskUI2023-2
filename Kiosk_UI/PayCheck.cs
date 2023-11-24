1bab9f4ce776824acf644c4086446d4fb6d2b2ec
KMZ150 <147787809+KMZ150@users.noreply.github.com>
2023-11-24 10:53:46 +0900

    메뉴 클릭 애니메이션

--- Kiosk_UI/PayCheck.cs
+++ Kiosk_UI/PayCheck.cs
@@ -10,6 +10,7 @@ namespace Kiosk_UI
 
         public PayCheck()
         {
+            this.KeyPreview = true;
             InitializeComponent();
         }
 
@@ -69,6 +70,26 @@ namespace Kiosk_UI
 
             //this.Hide();
             //takeout.ShowDialog();
+        }
+
+        private void PayCheck_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                this.Hide();
+                TakeoutForm takeout = new TakeoutForm();
+
+                DialogResult result1 = takeout.ShowDialog();
+                this.Hide();
+                if (result1 == DialogResult.OK)
+                {
+                    this.Show();
+                }
+                else
+                {
+                    this.Close();
+                }
+            }
         }
     }
 }
