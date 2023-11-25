b42e0053ee0a409678ea1d02671de3a79dceebbf
KMZ150 <147787809+KMZ150@users.noreply.github.com>
2023-11-25 18:38:25 +0900

    test

--- Kiosk_UI/PayCheck.cs
+++ Kiosk_UI/PayCheck.cs
@@ -29,6 +29,7 @@ namespace Kiosk_UI
         }
         public PayCheck()
         {
+            this.KeyPreview = true;
             InitializeComponent();
 
             this.AcceptButton = Yesbutton;
@@ -93,7 +94,25 @@ namespace Kiosk_UI
             //takeout.ShowDialog();
         }
 
-      
+        private void PayCheck_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                this.Hide();
+                TakeoutForm takeout = new TakeoutForm();
+
+                DialogResult result1 = takeout.ShowDialog();
+                this.Hide();
+                if (result1 == DialogResult.OK)
+                {
+                    this.Show();
+                }
+                else
+                {
+                    this.Close();
+                }
+            }
+        }
     }
 }
 
