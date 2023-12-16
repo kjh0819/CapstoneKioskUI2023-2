7ccdfeb0c924bacd1f92291cc6a41da8096b0217
kjh0819 <107752598+kjh0819@users.noreply.github.com>
2023-12-16 16:24:15 +0900

    파이썬 스크립트 주석 추가 및 불필요한 파일 제거

--- UpdateToServer.py
+++ UpdateToServer.py
@@ -1,3 +1,4 @@
+#파이썬 패키지 의존성 예외처리
 try:
     import paramiko
     import filecmp
@@ -10,6 +11,7 @@ except:
     import filecmp
     import zipfile
 
+# 프로그램 업데이트 종류 설정 함수
 def releaseOrDebug():
     print("1. 디버그 \n2. 릴리즈")
     debugOrRelease = input("값을 입력하세요 :")
@@ -21,12 +23,14 @@ def releaseOrDebug():
         print("올바른 값을 입력하세요.")
         return 0
 
+# 서버 주소 설정
 host = "capstonekiosk.koreacentral.cloudapp.azure.com"
 port = 22
 
 username = input("username을 입력하세요.: ")
 password = input("password를 입력하세요.: ")
 
+#서버에 3회 로그인 시도
 count=3
 while(count):
     try:
@@ -40,13 +44,14 @@ while(count):
         count-=1
 if count == 0:
     print("에러가 발생하였습니다. username,password를 확인하세요.")
+# 로그인 성공시
 else:
     directory_path=0
     while(directory_path == 0):
         directory_path=releaseOrDebug()
         print("선택된 디렉토리 경로:", directory_path)
     print("키오스크 실행파일 압축 시작")
-    
+#키오스크 프로그램 압축
     output_zip_path =  "Kiosk.zip"
     with zipfile.ZipFile(output_zip_path, "w") as zipFile:
         for root, dirs, files in os.walk(directory_path):
@@ -56,13 +61,14 @@ else:
                 zipFile.write(file_path, arcname=os.path.join(os.path.basename(directory_path), rel_path), compress_type=zipfile.ZIP_DEFLATED)
     print("업로드 시작")
     ssh = paramiko.SSHClient()
+#sftp를 이용하여 서버로 소프트웨어 업데이트
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh.connect(hostname=host, username=username, password=password)
     sftp = paramiko.SFTPClient.from_transport(transport)
     sftp.put("Kiosk.zip","Kiosk.zip")
-
+# ssh를 이용하여 서버에 업로드된 키오스크 소프트웨어의 hash값 수정
     stdin, stdout, stderr = ssh.exec_command('sh /home/azureuser/makeHash.sh')
-    # 결과 출력
+# 서버 스크립트 결과 출력
     print(stdout.read().decode())
     print(stderr.read().decode())
     ssh.close()
--- update.py
+++ update.py
@@ -1,3 +1,4 @@
+#의존성 해결
 try:
     import paramiko
     import filecmp
@@ -9,28 +10,36 @@ except:
     import paramiko
     import filecmp
     import zipfile
-
+#주소 및 계정 설정
 host = "capstonekiosk.koreacentral.cloudapp.azure.com"
 port = 22
 username = "ftpuser"
 password = "itisanon"
+
+#키오스크 소프트웨어 존재여부 확인
 if os.path.exists("c:\Kiosk"):
     os.chdir(r'c:\Kiosk')
 else:
     os.mkdir(r"c:\Kiosk")
     os.chdir(r'c:\Kiosk')
 
+#서버 연결
 transport = paramiko.Transport((host, port))
 transport.connect(username=username, password=password)
 
 sftp = paramiko.SFTPClient.from_transport(transport)
 
+#서버에서 Hash다운로드
 remote_path = "KioskHash.txt"
 local_path = "hash.txt"
 sftp.get(remote_path, local_path)
+
+#프로그램의 초기실행이 아니라면
 if os.path.isfile("KioskHash.txt"):
+    #로컬과 클라우드의 hash값 비교
     if filecmp.cmp(remote_path, local_path):
         print("프로그램이 최신버전입니다.")
+    #만약 해쉬값이 다르다면
     else:
         print("업데이트를 시작합니다.")
         transport = paramiko.Transport((host, port))
@@ -44,6 +53,7 @@ if os.path.isfile("KioskHash.txt"):
         print("압축을 해제합니다.")
         with zipfile.ZipFile('Kiosk.zip', 'r') as zip_file:
             zip_file.extractall()
+#프로그램의 초기 실행 이라면
 else:
     print("프로그램 초기 설정을 시작합니다.")
     transport = paramiko.Transport((host, port))
@@ -62,13 +72,16 @@ transport.close()
 
 print("프로그램을 시작합니다.")
 
+#키오스크 소프트웨어 실행
 import subprocess
 os.chdir(r'c:\Kiosk')
 subprocess.run(r'.\Kiosk_UI.exe', capture_output=True, text=True)
 
-
+#키오스크 소프트웨어가 종료 된 후
 os.chdir(r'c:\Kiosk')
 
+#위의 코드와 마찬가지로 업데이트 시행
+#함수화 요함
 transport = paramiko.Transport((host, port))
 transport.connect(username=username, password=password)
 
